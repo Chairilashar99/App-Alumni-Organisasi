d45543fb714132294be3f45bc76d19d745168c5e
chairil ashar <asharchairil4@gmail.com>
2023-06-28 21:20:03 +0700

    ftr layanan

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,13 @@
 import 'package:aplikasi_alumni_organisasi/theme.dart';
 import 'package:flutter/material.dart';
 
+List<Map<String, String>> layananList = [
+  {'title': 'Data Alumni', 'subtitle': 'Database Alumni'},
+  {'title': 'Berita & Kegiatan', 'subtitle': 'Kabar terkini seputar alumni'},
+  {'title': 'Donasi', 'subtitle': 'Informasi seputar Donasi'},
+  {'title': 'Aspirasi', 'subtitle': 'Suara Alumni'},
+];
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -108,7 +115,7 @@ class HomePage extends StatelessWidget {
                   left: 26,
                 ),
                 child: Image.asset('assets/icon_fitur.png'),
-                height: 12,
+                height: 16,
               ),
               SizedBox(
                 width: 4,
@@ -123,7 +130,217 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ),
+              Container(),
             ],
+          ),
+        ],
+      );
+    }
+
+    //Widget fitur Layanan
+    Widget fiturLayanan() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 11,
+          left: 12,
+          right: 12,
+        ),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(15)),
+        child: Column(
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.all(4),
+              title: Text(
+                layananList[0]['title'] ?? '',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: semiBold,
+                ),
+              ),
+              subtitle: Text(
+                layananList[0]['subtitle'] ?? '',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 12,
+                ),
+              ),
+              leading: Stack(children: [
+                Container(
+                  margin: EdgeInsets.only(left: 33),
+                  height: 48,
+                  child: Image.asset('assets/icon_elipse.png'),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5, left: 28),
+                  height: 42,
+                  child: Image.asset('assets/icon_alumni.png'),
+                )
+              ]),
+              trailing: Container(
+                margin: EdgeInsets.only(right: 35),
+                height: 11,
+                child: Image.asset('assets/icon_arrow.png'),
+              ),
+              onTap: () {},
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 13),
+              child: Divider(
+                color: Color(0xffE0E0E0),
+              ),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.all(4),
+              title: Text(
+                layananList[1]['title'] ?? '',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: semiBold,
+                ),
+              ),
+              subtitle: Text(
+                layananList[1]['subtitle'] ?? '',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 12,
+                ),
+              ),
+              leading: Stack(children: [
+                Container(
+                  margin: EdgeInsets.only(left: 33),
+                  height: 48,
+                  child: Image.asset('assets/icon_elipse.png'),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5, left: 28),
+                  height: 42,
+                  child: Image.asset('assets/icon_kegiatan.png'),
+                )
+              ]),
+              trailing: Container(
+                margin: EdgeInsets.only(right: 35),
+                height: 11,
+                child: Image.asset('assets/icon_arrow.png'),
+              ),
+              onTap: () {},
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 13),
+              child: Divider(
+                color: Color(0xffE0E0E0),
+              ),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.all(4),
+              title: Text(
+                layananList[2]['title'] ?? '',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: semiBold,
+                ),
+              ),
+              subtitle: Text(
+                layananList[2]['subtitle'] ?? '',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 12,
+                ),
+              ),
+              leading: Stack(children: [
+                Container(
+                  margin: EdgeInsets.only(left: 33),
+                  height: 48,
+                  child: Image.asset('assets/icon_elipse.png'),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5, left: 28),
+                  height: 42,
+                  child: Image.asset('assets/icon_donasi.png'),
+                )
+              ]),
+              trailing: Container(
+                margin: EdgeInsets.only(right: 35),
+                height: 11,
+                child: Image.asset('assets/icon_arrow.png'),
+              ),
+              onTap: () {},
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 13),
+              child: Divider(
+                color: Color(0xffE0E0E0),
+              ),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.all(4),
+              title: Text(
+                layananList[3]['title'] ?? '',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: semiBold,
+                ),
+              ),
+              subtitle: Text(
+                layananList[3]['subtitle'] ?? '',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 12,
+                ),
+              ),
+              leading: Stack(children: [
+                Container(
+                  margin: EdgeInsets.only(left: 33),
+                  height: 48,
+                  child: Image.asset('assets/icon_elipse.png'),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5, left: 28),
+                  height: 42,
+                  child: Image.asset('assets/icon_aspirasi.png'),
+                )
+              ]),
+              trailing: Container(
+                margin: EdgeInsets.only(right: 35),
+                height: 11,
+                child: Image.asset('assets/icon_arrow.png'),
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      );
+    }
+
+    //Widget Pengumuman Title
+    Widget pengumumanTitle() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: 21,
+              left: 26,
+            ),
+            child: Row(
+              children: [
+                Container(
+                  height: 16,
+                  child: Image.asset('assets/icon_pengumuman.png'),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  'Pengumuman',
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: 21,
+              right: 26,
+            ),
+            child: Text(
+              'Lihat semua',
+            ),
           ),
         ],
       );
@@ -133,6 +350,8 @@ class HomePage extends StatelessWidget {
       children: [
         poster(),
         fiturLayananTitle(),
+        fiturLayanan(),
+        pengumumanTitle(),
       ],
     );
   }
